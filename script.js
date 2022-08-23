e27255bd7078bae3354e8c545414b300233c0257
aanchalsharmaa1024 <101443787+aanchalsharmaa1024@users.noreply.github.com>
2022-08-23 10:22:08 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const selectImage = document.querySelector('.select-image'); 
-const inputFile = document.querySelector('#file');    
+const inputFile = document.querySelector('#file');     
 const imgArea = document.querySelector('.img-area');   
 selectImage.addEventListener('click', function () {
 	inputFile.click();
